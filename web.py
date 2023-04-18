3b68fe6559b2d42cc8234188e311008d9519af86
mrkicrade <mrkic78@gmail.com>
2023-04-18 16:24:58 +0200

    Implement "Add new todo items" feature.

--- web.py
+++ web.py
@@ -5,6 +5,12 @@ import functions
 # aplikaciju pokreni iz terminala komandom streamlit run web.py
 
 
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 todos = functions.get_todos()
 
 st.title('My Todo App')
@@ -14,6 +20,6 @@ st.write('This add is to increase your productivity')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Add new todo...')
+st.text_input(label='', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
 
 
