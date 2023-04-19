ddd835e70492d73dbdbfce35061dd6453bda6142
mrkicrade <mrkic78@gmail.com>
2023-04-19 08:48:29 +0200

    Day 5 bonus, bfe

---
+++ bug-fixing_exercises/bfe5/bfe5.py
@@ -0,0 +1,22 @@
+# Exercise 1
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# user_choice = int(input("Enter the index of the item: "))
+#
+# message = f"You chose {menu[user_choice]}."
+# print(message)
+
+# Exercise 2
+
+# menu = ["pasta", "pizza", "salad"]
+#
+# for i, j in enumerate(menu):
+#     print(f"{i}.{j}")
+
+# Exercise 3
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
